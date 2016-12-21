a4d178d9936c22a5888d203d0a75c3b11c56c204
Justin Walker <jwalker@christianbook.com>
2016-12-21 13:30:30 -0500

    add layout and bluebird for redis promises

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
-module.exports = (app, redis) => {
+module.exports = (app, db) => {
 	app.get('/', (req, res) => {
-		res.render('index');
+		res.redirect('/home/justin');
 	});
-	app.get('/test', (req, res) => {
-		res.send("testing");
+	
+	app.get('/home/:name', (req, res) => {
+		var nameExists = ( name ) => { return db.getAsync(name) },
+			setName = ( name ) => { return db.setAsync( 'name', name ) },
+			username = req.params.name ? (req.params.name[0].toUpperCase() + req.params.name.substring(1)) : 'noname';
+		
+		nameExists('name').then( 
+			(name_exists) => { 
+				if ( name_exists && name_exists == username ){ 
+					res.render('index', { myname: username, exists: true});
+				} else { 
+					setName(username).then(()=>res.render('index', {myname: username})); 
+				}
+			}
+		);
 	});
 };
--- server.js
+++ server.js
@@ -7,8 +7,13 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let session = require('express-session');
 let _redis = require('redis');
-let redis = _redis.createClient("6379", 'redis');
-redis.on('connect', () => { console.log("CONNECTED TO REDIS") })
+
+let bluebird = require('bluebird');
+bluebird.promisifyAll(require('redis'));
+
+let db = _redis.createClient("6379", 'redisnode');
+db.on('connect', () => { console.log("\nCONNECTED TO REDIS DB\n") })
+
 app.use(cookieParser());
 app.use(session({secret: "catParty"}));
 
@@ -16,13 +21,12 @@ app.use(session({secret: "catParty"}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.set('redis', redis);
 
 // set static file directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public',express.static(path.join(__dirname, 'public')));
 
-require('./routes/index')(app, redis);
-
+require('./routes/index')(app, db);
+app.get('/favicon.ico', (req, res) => { res.send(200); } );
 
 app.listen(PORT);
 console.log(`App is running on port ${PORT}`);
